1d330441d8f96bc526c118d17749e42774561ea1
secilunveren <secilunveren@gmail.com>
2022-08-16 23:45:10 +0300

    Create hamburger menu for the mobile screens

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi, welcome to my portfolio website!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if ($(window).width() < 640 && x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
