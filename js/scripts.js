a3c6640193f10515a9eefc7c07262d0d18ffa83c
secilunveren <secilunveren@gmail.com>
2022-08-19 20:26:47 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,14 @@ function menuToggle() {
   } else {
     x.className = 'navtoggle';
   }
+
+
+        var icon=document.getElementsByClassName("fa-bars")[0];
+    if(icon){
+       icon.className = icon.className.replace("fa-bars", "fa-xmark");
+    }
+    else{
+    icon=document.getElementsByClassName("fa-xmark")[0];
+     icon.className = icon.className.replace("fa-xmark", "fa-bars");
+    }
 }
